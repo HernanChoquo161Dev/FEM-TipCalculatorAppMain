00932266993d360fd3c122bc9cc130e9ab2b635d
Hernan <hernanchoquedev@gmail.com>
2021-10-12 19:51:27 -0300

    refactory code - HTML - CSS - JS

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ const resetOption = document.querySelector(".output-amount__reset");
 const tipCustomOption = document.querySelector(".tip-grid__text-option-tip");
 
 var optionSelect = -1;
+var firstInput = [true, true];
+var customFirstInput = true;
+
+resetOption.disabled = true;
 
 function tipPerOption(optionSelect){
     if (optionSelect == 0) return 0.05;
@@ -15,63 +19,71 @@ function tipPerOption(optionSelect){
 }
 
 function activeCalculator(singleInputs, tipCustomOption, optionSelect){
-    let checkInputs = true;
-    let checkOption = true;
-    let checkCustomOption = true;
-
-    singleInputs.forEach(element => {
-        if(element.value.length == 0){
-            checkInputs = false;
-        }
-    });
+    if(singleInputs[0].value.length == 0) return 0;
+    if(singleInputs[1].value.length == 0) return 1;
 
     if(optionSelect == -1){
-        if(tipCustomOption.value.length <= 0) {
-            checkOption = false;
-            checkCustomOption = false;
+        if(tipCustomOption.value.length == 0) {
+            return 2;
         }
     }
 
-    return checkInputs && checkOption && checkCustomOption;
+    return -1;
 }
 
-function roundToTwo(num) {    
-    return +(Math.round(num + "e+2")  + "e-2");
+function roundToTwo(number) {    
+    return +(Math.round(number + "e+2")  + "e-2");
 }
 
 function calculateTip(singleInputs, outputAmounts, optionSelect){
     let paymentPerPeople = parseFloat(singleInputs[0].value) / singleInputs[1].value;
 
-    if(tipCustomOption.value.length == 0){
-        let tipPerPeople = paymentPerPeople * tipPerOption(optionSelect);
-        let totalTipPerPeople = paymentPerPeople + tipPerPeople;
-    
-        outputAmounts[0].textContent = "$" + roundToTwo(tipPerPeople);
-        outputAmounts[1].textContent = "$" + roundToTwo(totalTipPerPeople);
+    let tipPerPeople;
+    let totalTipPerPeople;
+
+    if(optionSelect != -1){
+        tipPerPeople = paymentPerPeople * tipPerOption(optionSelect);
     }
     else{
-        let tipPerPeople = paymentPerPeople * parseFloat(tipCustomOption.value / 100);
-        let totalTipPerPeople = paymentPerPeople + tipPerPeople;
-    
-        outputAmounts[0].textContent = "$" + roundToTwo(tipPerPeople);
-        outputAmounts[1].textContent = "$" + roundToTwo(totalTipPerPeople);
+        tipPerPeople = paymentPerPeople * parseFloat(tipCustomOption.value / 100);
     }
+    totalTipPerPeople = paymentPerPeople + tipPerPeople;
+
+    outputAmounts[0].textContent = "$" + roundToTwo(tipPerPeople);
+    outputAmounts[1].textContent = "$" + roundToTwo(totalTipPerPeople);
+}
+
+function changeColorOption(optionGridSelect, colorBack, color){
+    optionGridSelect.style.backgroundColor = colorBack;
+    optionGridSelect.style.color = color;
+}
+
+function activateAlertTextInput(singleInputsPerson){
+    let textAlert = document.querySelector(".single-input__text-alert");
+            
+    singleInputsPerson.classList.toggle("single-input__input--alert");
+    textAlert.classList.toggle("single-input__text-alert--active");
 }
 
 function addEventClickGridOption(optionGrid, singleInputs, outputAmounts, resetOption, tipCustomOption, index){
     optionGrid[index].addEventListener('click', function (event) {
 
-        optionGrid[index].style.backgroundColor = "hsl(172, 67%, 45%)";
-        optionGrid[index].style.color = "hsl(183, 100%, 15%)";
+        /*style change of a selected option*/
+        changeColorOption(optionGrid[index], "hsl(172, 67%, 45%)", "hsl(183, 100%, 15%)");
 
         if(optionSelect != -1){
-            optionGrid[optionSelect].style.backgroundColor = "hsl(183, 100%, 15%)";
-            optionGrid[optionSelect].style.color = "hsl(0, 0%, 100%)";
+            changeColorOption(optionGrid[optionSelect], "hsl(183, 100%, 15%)", "hsl(0, 0%, 100%)");
         }
-
         optionSelect = index;
 
-        if(activeCalculator(singleInputs, tipCustomOption,optionSelect)){
+        /*validation data*/
+
+        let validationInput = activeCalculator(singleInputs, tipCustomOption,optionSelect);
+
+        if(validationInput == -1){
+            if(singleInputs[1].classList.contains("single-input__input--alert"))
+                activateAlertTextInput(singleInputs[1]);
+
             calculateTip(singleInputs, outputAmounts, optionSelect);
 
             if(resetOption.disabled){
@@ -79,12 +91,14 @@ function addEventClickGridOption(optionGrid, singleInputs, outputAmounts, resetO
                 resetOption.style.backgroundColor = "hsl(172, 67%, 45%)";
             }
         }
+        else if(validationInput == 1){
+            if(!singleInputs[1].classList.contains("single-input__input--alert"))      
+                activateAlertTextInput(singleInputs[1]);
+        }
     });
-  }
+}
 
-var firstInput = [true, true];
-
-function numericalInputValidation(elementInputs, enabledPoint){
+function numericalInputValidation(elementInputs, enabledPoint, enabledArrows){
     elementInputs.onkeydown = function(e) {
 
         if(e.which == 8) return true;
@@ -92,13 +106,24 @@ function numericalInputValidation(elementInputs, enabledPoint){
             if(e.which == 190 || e.which == 110)
                 return true;
         }
+        if(enabledArrows) 
+            if(e.which == 37 || e.which == 38 || e.which == 39 || e.which == 40)
+                return true;
         if(isNaN(parseInt(e.key))) return false;
     };
+}
+
+function desactiveOptionReset(optionReset){
+    if(!optionReset.disabled){
+        optionReset.disabled = true;
+        optionReset.style.backgroundColor = "hsla(172, 67%, 45%, 0.5)";
+    }
 }
 
 function addEventInputGridOption(index){
     singleInputs[index].addEventListener('input', function (event) {
 
+        /*Fix bug caused by first input character*/
         if(event.target.value.length == 1 && firstInput){
             if(isNaN(parseInt(event.target.value))){
                 event.target.value = "";
@@ -106,13 +131,33 @@ function addEventInputGridOption(index){
                 firstInput[index] = false;
             }
         }
+
+        /*control alert message and data insufficiency*/
+        if(event.target.value.length > 0){
+            if(singleInputs[1].classList.contains("single-input__input--alert")){
+                activateAlertTextInput(singleInputs[1]);
+            }
+        }
+        else{
+            outputAmounts.forEach(element => {
+                element.textContent = "$0.00";
+            });
+
+            desactiveOptionReset(resetOption);
+        }
+
+        /*check inputs*/
     
         if(index == 0)
-            numericalInputValidation(singleInputs[index], true);
+            numericalInputValidation(singleInputs[index], true, true);
         else 
-            numericalInputValidation(singleInputs[index], false);
+            numericalInputValidation(singleInputs[index], false, true);
 
-        if(activeCalculator(singleInputs, tipCustomOption,optionSelect)){
+        /*validation data*/
+            
+        let validationInput = activeCalculator(singleInputs, tipCustomOption,optionSelect);
+
+        if(validationInput == -1){
             calculateTip(singleInputs, outputAmounts, optionSelect);
             
             if(resetOption.disabled){
@@ -120,22 +165,26 @@ function addEventInputGridOption(index){
                 resetOption.style.backgroundColor = "hsl(172, 67%, 45%)";
             }
         }
-        else{
-            outputAmounts.forEach(element => {
-                element.textContent = "$0.00";
-            });
+        else if(validationInput == 0){
+            if(singleInputs[1].classList.contains("single-input__input--alert")){
+                activateAlertTextInput(singleInputs[1]);
+            }
+        }
+        else if(validationInput == 1){
+            if(!singleInputs[1].classList.contains("single-input__input--alert")){
+                activateAlertTextInput(singleInputs[1]);
+            }
         }
     });
 }
 
-resetOption.disabled = true;
-
-var customFirstInput = true;
-
 tipCustomOption.addEventListener('input', function (event) {
+    /*check inputs*/
 
-    numericalInputValidation(tipCustomOption, true);
+    numericalInputValidation(tipCustomOption, true, true);
     
+    /*change styles custom tip option*/
+
     if(!customFirstInput){
         tipCustomOption.style.textAlign = "right";
     }
@@ -143,6 +192,8 @@ tipCustomOption.addEventListener('input', function (event) {
     if(event.target.value.length == 0){
         tipCustomOption.style.textAlign = "center";
     }
+
+    /*Fix bug caused by first input character*/
 
     if(event.target.value.length == 1 && customFirstInput){
         if(isNaN(parseInt(event.target.value))){
@@ -152,14 +203,22 @@ tipCustomOption.addEventListener('input', function (event) {
         }
     }
 
-    if(event.target.value.length > 0 && optionSelect != -1){
-        gridOptions[optionSelect].style.backgroundColor = "hsl(172, 67%, 45%)";
-        gridOptions[optionSelect].style.color = "hsl(183, 100%, 15%)";
+    /*change styles tip custom option and option selected */
 
-        optionSelect = -1;
+    if(event.target.value.length > 0){
+        if(optionSelect != -1){
+            changeColorOption(gridOptions[optionSelect], "hsl(183, 100%, 15%)", "hsl(0, 0%, 100%)");
+
+            optionSelect = -1;
+        }
+    }
+    else{
+        desactiveOptionReset(resetOption);
     }
 
-    if(activeCalculator(singleInputs, tipCustomOption,optionSelect)){
+    /*validation data */
+
+    if(activeCalculator(singleInputs, tipCustomOption,optionSelect) == -1){
         calculateTip(singleInputs, outputAmounts, optionSelect);
 
         if(resetOption.disabled){
@@ -174,15 +233,7 @@ tipCustomOption.addEventListener('input', function (event) {
     }
 });
 
-resetOption.addEventListener('click', function (event) {
-    /*singleInputs.forEach(
-        function(currentValue, currentIndex, listObj) {
-          console.log(currentValue + ', ' + currentIndex + ', ' + this);
-          currentValue.value = "";
-        },
-        'miEsteArg'
-      );*/
-
+function resetCalculator(singleInputs, gridOptions, outputAmounts, tipCustomOption, resetOption, optionSelect){
     singleInputs.forEach(element => {
         element.value = "";
     });
@@ -190,13 +241,21 @@ resetOption.addEventListener('click', function (event) {
     outputAmounts.forEach(element => {
         element.textContent = "$0.00";
     });
+    
+    tipCustomOption.value = "";
 
     if(optionSelect != -1){
         gridOptions[optionSelect].style.backgroundColor = "hsl(183, 100%, 15%)";
         gridOptions[optionSelect].style.color = "hsl(0, 0%, 100%)";
     }
 
+    desactiveOptionReset(resetOption);
+
     optionSelect = -1;
+}
+
+resetOption.addEventListener('click', function (event) {
+    resetCalculator(singleInputs, gridOptions, outputAmounts, tipCustomOption, resetOption, optionSelect);
 });
 
 for(b = 0; b < gridOptions.length; b++){
